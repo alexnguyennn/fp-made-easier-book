0137fa484cf84609c72b33f7448fe06cd98fbc48
Alex Nguyen <alexnguyen.dev@tutanota.com>
2021-10-27 21:57:40 +1100

    implement apply/flipped with operators

--- src/Ch5.purs
+++ src/Ch5.purs
@@ -2,8 +2,7 @@ module Ch5 where
 
 import Effect (Effect)
 import Effect.Console (log)
-import Prelude hiding (flip)
-import Prelude (Unit, show, const) 
+import Prelude (Unit, discard, show)
 
 -- flip :: ∀ a b c. (a -> b -> c) -> b -> a -> c
 -- flip f x y = f y x
@@ -13,6 +12,23 @@ flip f = \x y -> f y x
 -- flip :: ∀ a b c. (a -> b -> c) -> b -> (a -> c) 
 -- flip f x = \y -> f y x
 
+-- It can also be thought of as creating a function that ignores its argument
+-- useful to use partial application to 'freeze' a predetermined value that will be returned regardless of next input
+const :: ∀ a b . a -> b -> a
+const x _ = x
+
+apply :: ∀ a b. (a -> b) -> a -> b
+apply f x = f x
+
+infixr 0 apply as $
+
+applyFlipped :: ∀ a b. a -> (a -> b) -> b
+-- applyFlipped x f = f x
+applyFlipped = flip apply
+
+infixl 1 applyFlipped as #
+
 test:: Effect Unit
 test = do
-    log (show (flip const 1 2))
+    log $ show $ flip const 1 2
+    flip const 1 2 # show # log
